208ece64c465c76ce73b8136dd7d06cf39d120cb
harvaaro <harvaaro@isu.edu>
2019-02-09 17:14:44 -0700

    Finished commenting the simulation with what it is doing at the stages. And removed my debug prints from the test.

--- src/main/java/edu/isu/cs/cs3308/Simulation.java
+++ src/main/java/edu/isu/cs/cs3308/Simulation.java
@@ -57,18 +57,30 @@ public class Simulation {
 
     /**
      * Executes the Simulation
-     * 1: Store the current time (currentTimer) in the queue as that persons enter time.
-     * 2: When they leave get the: currentTimer - that persons time.
-     * 3: Add that number to the total amount of wait time (queueWaitedTime)
-     * 4: Add to person counter to know how many have gone through (countPeopleDone)
-     * 5: Once minuteTimer = dailyMinutes: queueWaitedTime / countPeopleDone
-     * 6: Add that to a the average wait time per iteration (iterateWaitTime)
-     * 7: Repeat the steps 1-6 (numIterations) more times
-     * 8: Get final average wait time as: iterateWaitTime / numIterations
+	 * 01: Print out the (arrivalRate) to show how many will be added to the queues
+	 * 02: For each number of queues (numQueue) up to and including the (maxNumQueues) loop
+	 * 		This is for doing each amount of queues, in our case 1 through 10
+	 * 03: For each number of loops (numLoop) up to the total iterations (numIterations) loop
+	 * 		This is for doing the test multiple times to get a good average, in our case 50
+	 * 04: Create the number of queues needed (allList) based off of the (numQueue)
+	 * 05: For each current minute (currentTimer) up to and including the daily amount (dailyMinutes) loop
+	 * 		This is for adding and removing people to the queue, in our case through 720 minutes
+	 * 06: Add a random number of people to the lineup based on the (arrivalRate)
+	 * 		This also makes sure each person joins the shortest queue as the are added
+	 * 		Also stores the currentTimer as the value in that queue position for later comparison
+	 * 07: Remove 2 people from each queue, or 1 if there is only 1, or none if the queue is empty
+     * 		When they leave get the: currentTimer - that persons time.
+     * 		Add that number to the total amount of wait time (queueWaitedTime)
+     * 		Add to person counter to know how many have gone through (countPeopleDone)
+     * 08: Once minuteTimer = dailyMinutes: queueWaitedTime / countPeopleDone
+     * 		Add that to a the average wait time per iteration (iterateWaitTime)
+     * 09: Repeat the steps 3-8 (numIterations) more times
+     * 10: Print final average wait time as: iterateWaitTime / numIterations
+	 * 11: Reset a few variables to not carry over values into the next loop
+	 * 12: Repeat the steps 2-11 (maxNumQueues) more times
      */
     public void runSimulation() {
-        // for some reason needed this line to make the testResults work otherwise it skipped #1
-        System.out.println();
+        System.out.println("Arrival rate: " + arrivalRate);
 
         for (numQueue = 1; numQueue <= maxNumQueues; numQueue++) {
             for (int numLoop = 0; numLoop < numIterations; numLoop++) {
@@ -91,7 +103,10 @@ public class Simulation {
         }
     }
 
-    private void createQueueAmount() {
+	/**
+	 * Creates the needed array size for the amount of queues to use
+	 */
+	private void createQueueAmount() {
         allLines = new LinkedQueue[numQueue];
 
         for (int makeLanes = 0; makeLanes < numQueue; makeLanes++) {
@@ -99,19 +114,24 @@ public class Simulation {
         }
     }
 
-    private void addPeopleToQueues() {
-        int minutePeople = getPeoplePerMinute();
+	/**
+	 * Gets a random number of people that entered in that minute
+	 * Inserts those people into the shortest queue per person
+	 */
+	private void addPeopleToQueues() {
+        int minutePeople = getRandomNumPeople(arrivalRate);
 
         for (int person = 0; person < minutePeople; person++) {
             addPersonToShortest();
         }
     }
 
-    private int getPeoplePerMinute() {
-        return getRandomNumPeople(arrivalRate);
-    }
-
-    private void addPersonToShortest() {
+	/**
+	 * Iterates through the array to find the shortest queue
+	 * Then adds the current time to that queue, to represent
+	 * A new person being added to that queue
+	 */
+	private void addPersonToShortest() {
         int minSize = allLines[0].size();
         int minIndex = 0;
 
@@ -129,7 +149,12 @@ public class Simulation {
         allLines[minIndex].offer(currentTimer-1);
     }
 
-    private void removeTwoFromEach() {
+	/**
+	 * Iterates through the array of queues and remove 2 people form each
+	 * If there is only 1 person it only removes 1, otherwise none
+	 * Adds to the wait time tally, and increments a count of people
+	 */
+	private void removeTwoFromEach() {
         for (int whichQueue = 0; whichQueue < numQueue; whichQueue++) {
             int queueSize = allLines[whichQueue].size();
 
